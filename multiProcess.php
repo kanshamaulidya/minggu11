29d43409de34a7e85e21aa8baa62a5071f409e71
Kansha Maulidya Shyfa <kanshalidya12@gmail.com>
2022-05-17 23:32:36 +0700

    Kansha - Minggu11

---
+++ multiProcess.php
@@ -0,0 +1,18 @@
+<?php
+    include "myconnection.php";
+
+    $username = $_POST["username"];
+    $password = md5($_POST["password"]);
+
+    $query = "SELECT * FROM user WHERE username = '$username' && password = '$password'";
+    $result = mysqli_query($connect, $query);
+    $check = mysqli_num_rows($result);
+
+    if($row['level']=="1"){
+        header("Location:homeCRUD.php");
+    } else if ($row['level']=="2"){
+        header("Location:homeGuest.html");
+    }else{
+        header("Location:loginForm.php?error=gagal");
+    }
+?>
